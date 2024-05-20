db5231f588978a08f36ca2747902e747bb74aca5
Ташимбетов Тимур Нуралиевич <ttashimbetov@nic.etu>
2024-05-20 10:41:34 +0300

    очистка ветки

--- src/functions/getDateString.js
+++
@@ -1,5 +0,0 @@
-export default function getDateString(date) {
-  const day = date.getDate();
-  const month = date.toLocaleString("ru-RU", { month: "long" });
-  return `${day} ${month}`;
-}
--- src/pages/FoodsPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="marginDrawer paddingContent">
-    <FoodTable></FoodTable>
-  </div>
-</template>
-<script setup>
-import FoodTable from "../components/FoodTable.vue";
-</script>
-<style></style>
